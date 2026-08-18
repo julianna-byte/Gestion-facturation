1a986e5a7ae8ae87a669209925eb462dcdddbb95
Dame Julienne TCHAMBA <jtchamba02@gmail.com>
2026-08-18 22:15:33 +0000

    Definition d'utilisateurs pour test

---
+++ Gestion-facturation/src/main/java/Configuration/DataInitializer.java
@@ -0,0 +1,43 @@
+package Configuration;
+
+import Entity.Role;
+import Entity.Utilisateur;
+import Repository.UtilisateurRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+     private final UtilisateurRepository utilisateurRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public DataInitializer(UtilisateurRepository utilisateurRepository, PasswordEncoder passwordEncoder) {
+        this.utilisateurRepository = utilisateurRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+     @Override
+    public void run(String... args) {
+        if (utilisateurRepository.findByIdentifiant("admin").isEmpty()) {
+            Utilisateur admin = new Utilisateur();
+            admin.setIdentifiant("admin");
+            admin.setMotdepasse(passwordEncoder.encode("admin123"));
+            admin.setRole(Role.ADMIN);
+            utilisateurRepository.save(admin);
+            System.out.println("Utilisateur admin créé avec succès.");
+        }
+
+            if (utilisateurRepository.findByIdentifiant("commercial").isEmpty()) {
+            Utilisateur commercial = new Utilisateur();
+            commercial.setIdentifiant("commercial");
+            commercial.setMotdepasse(passwordEncoder.encode("commercial123"));
+            commercial.setRole(Role.COMMERCIAL);
+            utilisateurRepository.save(commercial);
+            System.out.println("Utilisateur commercial créé avec succès.");
+        }
+    }
+    
+
+}
